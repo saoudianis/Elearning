92f17fdaaaa482f098f3a2f3ba79eb0147f6daa7
saoudianis <anismax333@gmail.com>
2020-06-04 18:33:22 +0100

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,61 @@
+
+function changerfr()
+{
+    document.getElementById("step1").innerHTML = 'La Premiére étape : Choisissez le nom de la Wilaya';
+    document.getElementById("step2").innerHTML = 'La Deuxiéme étape : Cliquez sur celui dans lequel vous effectuez des études supérieures';
+    document.getElementById("C1").innerHTML = 'Choisissez';
+    document.getElementById("C2").innerHTML = 'Choisissez';
+    document.getElementById("info").innerHTML = 'Tous les liens vers le-learning au niveau national et le Ministère de lEnseignement Supérieur et de la Recherche Scientifique';
+    document.getElementById("view").innerHTML = 'Afficher les résultats';
+    document.getElementById("thnx").innerHTML = 'Remerciements particuliers à :';
+    document.getElementById("copyright").innerHTML = '2020 © Copyright :Ministère de lEnseignement Supérieur et de la Recherche Scientifique';
+   
+}
+
+function changerar()
+{
+    document.getElementById("step1").innerHTML = 'الخطوة الاولى : إختر الولاية';
+    document.getElementById("step2").innerHTML = 'الخطوة الثانية : إختر الجهة التي تزاول فيها التعليم العالي';
+    document.getElementById("C1").innerHTML = 'إختر من هنا';
+    document.getElementById("C2").innerHTML = 'إختر من هنا';
+    document.getElementById("info").innerHTML = 'كل روابط التعلم الإلكتروني على المستوى الوطني وزارة التعليم العالي و البحث العلمي';
+    document.getElementById("view").innerHTML = 'عرض النتائج';
+    document.getElementById("thnx").innerHTML = 'شكر خاص لــ :';
+    document.getElementById("copyright").innerHTML = 'كافة الحقوق محفوظة : وزارة التعليم العالي و البحث العلمي © 2020';
+    
+}
+
+function changerfrp1()
+{document.getElementById("info").innerHTML = 'Tous les liens vers le-learning au niveau national et le Ministère de lEnseignement Supérieur et de la Recherche Scientifique';
+ document.getElementById("rzlt").innerHTML = 'Les sites nont pas encore été ajoutés, veuillez réessayer plus tard';
+ document.getElementById("thnx").innerHTML = 'Remerciements particuliers à :';
+    document.getElementById("copyright").innerHTML = '2020 © Copyright :Ministère de lEnseignement Supérieur et de la Recherche Scientifique';
+}
+
+function changerarp1()
+{
+    document.getElementById("info").innerHTML = 'كل روابط التعلم الإلكتروني على المستوى الوطني وزارة التعليم العالي و البحث العلمي';
+    document.getElementById("rzlt").innerHTML = 'لم يتم إضافة المواقع بعد يرجى المحاولة لاحقا';
+    document.getElementById("thnx").innerHTML = 'شكر خاص لــ :';
+    document.getElementById("copyright").innerHTML = 'كافة الحقوق محفوظة : وزارة التعليم العالي و البحث العلمي © 2020';
+}
+
+function darkmode()
+{
+    window.genb.style.backgroundColor="#414141";
+    window.header.style.backgroundColor="#010101";
+    window.mins.style.backgroundColor="#010101";
+    window.foot2.style.backgroundColor="#010101";
+    window.foot1.style.backgroundColor="#010101";
+    
+}
+
+function lightmode()
+{
+    window.genb.style.backgroundColor="#3B8BEB";
+    window.header.style.backgroundColor="#3B8BEB";
+    window.mins.style.backgroundColor="#B23850";
+    window.foot2.style.backgroundColor="#3B8BEB";
+    window.foot1.style.backgroundColor="#B23850";
+    
+}
